ad30e54b0b03e2b50da0a57cb405d8b1b291194c
Nic Barnes <colorpulse@gmail.com>
2020-08-24 16:25:13 +0200

    fixed map bug

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,6 +8,7 @@ class HomePage extends React.Component {
     books: [],
     filteredBooks: [],
     isLoading: true,
+    notFound:false
   };
   componentDidMount() {
     // console.log(this.props.firebase.auth.currentUser);
@@ -15,16 +16,14 @@ class HomePage extends React.Component {
   }
 
   fetchApiData = () => {
-    fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=search-terms`
-    )
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=search-terms&maxResults=40`)
       .then((allBooks) => allBooks.json())
       .then((allBooks) => {
         this.setState(
           {
             books: allBooks.items,
-            isLoading: false,
             filteredBooks: allBooks.items,
+            isLoading: false,
           },
           () => console.log(this.state.filteredBooks)
         );
@@ -36,23 +35,24 @@ class HomePage extends React.Component {
 
   handleFilter = (e) => {
     e.preventDefault();
-    let input = e.target.value || 'search-terms';
     
+    //Handle no input
+    let input = e.target.value || "technology";
 
-      fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${input}&langRestrict=en`
-      )
-        .then((filteredBooks) => filteredBooks.json())
-        .then((filteredBooks) => {
-         
-            this.setState({ filteredBooks: filteredBooks.items }, () =>
-            console.log(this.state.filteredBooks)
-          );
-          
-          
-        });
-    
-    
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${input}&langRestrict=en`
+    )
+      .then((filteredBooks) => filteredBooks.json())
+      .catch((err)=> {
+        this.setState({ filteredBooks: this.state.books.items });
+      })
+      .then((filteredBooks) => {
+        this.setState({ filteredBooks: filteredBooks.items }, () =>
+          console.log(this.state.filteredBooks)
+        );
+      })
+      
+      
   };
 
   render() {
@@ -69,19 +69,19 @@ class HomePage extends React.Component {
               onChange={this.handleFilter}
               placeholder="Search All Books..."
             ></input>
-          {this.state.filteredBooks ? 
-          this.state.filteredBooks.map((book, index) => {
-              return (
-                <img
-                  key={index}
-                  src={book.volumeInfo.imageLinks.smallThumbnail}
-                ></img>
-              )
-            }) : <p>No Books Found</p>
-            }
-            
-
-            
+            {this.state.filteredBooks ? (
+              this.state.filteredBooks.map((book, index) => {
+                return (
+                  <img
+                    alt={book.volumeInfo.title}
+                    key={index}
+                    src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail}
+                  ></img>
+                );
+              })
+            ) : (
+              <p>No Books Found</p>
+            )}
           </div>
         )}
       </AuthUserContext.Consumer>
