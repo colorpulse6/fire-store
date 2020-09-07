76f1ddcba3002601d63cb899e12efd01732f3f31
Nic Barnes <colorpulse@gmail.com>
2020-09-07 11:24:50 +0200

    added saved search funcionality

--- src/components/GoogleBooks/index.js
+++ src/components/GoogleBooks/index.js
@@ -4,18 +4,18 @@ import Input from "../input";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
 import LoadingGif from "../LoadingGif";
-import LocalStorage from '../LocalStorage';
+import LocalStorage from "../LocalStorage";
 class SearchBooks extends React.Component {
   state = {
     filteredBooks: [],
     loading: true,
     input: JSON.parse(localStorage.getItem("input")) || "",
     existingEntries: JSON.parse(localStorage.getItem("allEntries")) || [],
-
+    saved: false,
+    savedEntry: "",
   };
   componentDidMount() {
     this.getBooks(this.state.input);
-    
   }
 
   handleFilter = (e) => {
@@ -52,8 +52,34 @@ class SearchBooks extends React.Component {
   setSearchTerm = (input) => {
     this.setState({ input: input }, () => {
       localStorage.setItem("input", JSON.stringify(this.state.input));
-      
     });
+  };
+
+  handleSaveSearch = () => {
+    let allEntries = JSON.parse(localStorage.getItem("allEntries"));
+    if (!allEntries.includes(this.state.input)) {
+      this.setState(
+        {
+          existingEntries: [...this.state.existingEntries, this.state.input],
+          saved: true,
+          savedEntry:"Search Books..."
+        },
+        () => {
+          localStorage.setItem(
+            "allEntries",
+            JSON.stringify(this.state.existingEntries)
+          );
+          setTimeout(() => {
+            this.setState({ saved: false });
+          }, 1000);
+        }
+      );
+    }
+  };
+
+  handleSavedSearchLink = (entry) => {
+    this.setState({ savedEntry: entry });
+    this.getBooks(entry);
   };
 
   render() {
@@ -64,13 +90,18 @@ class SearchBooks extends React.Component {
       <div>
         <Input
           onChange={this.handleFilter}
-          placeholder={"Search Books..."}
+          placeholder={this.state.savedEntry || "Search Books..."}
         ></Input>
         <LocalStorage
           input={this.state.input}
+          isActive={this.state.isActive}
+          existingEntries={this.state.existingEntries}
+          saved={this.state.saved}
+          setisActive={this.setisActive}
+          handleSaveSearch={this.handleSaveSearch}
+          handleSavedSearchLink={this.handleSavedSearchLink}
         />
-        <p>Last Search: {this.state.input}</p>
-        
+
         <div className={BookStyles.container}>
           {this.state.filteredBooks
             ? this.state.filteredBooks.map((book, index) => {
--- src/components/LocalStorage.js
+++ src/components/LocalStorage.js
@@ -1,82 +1,60 @@
 import React, { Component } from "react";
-import ButtonStyles from '../constants/buttons.module.scss'
+import ButtonStyles from "../constants/buttons.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
-export default class LocalStorage extends Component {
-  state = {
-    existingEntries: JSON.parse(localStorage.getItem("allEntries")) || [],
-    input: "",
-    saved: false,
-  };
+export default function LocalStorage(props) {
+  const [isActive, setisActive] = React.useState(false);
 
-  handleSaveSearch = () => {
-    let allEntries = JSON.parse(localStorage.getItem("allEntries"));
-    if (!allEntries.includes(this.props.input)) {
-      this.setState(
-        { existingEntries: [...this.state.existingEntries, this.props.input], saved:true },
-        () => {
-          localStorage.setItem(
-            "allEntries",
-            JSON.stringify(this.state.existingEntries)
-            
-          );
-          setTimeout(() => {
-               this.setState({saved:false}) 
-        }, 1000);
-        }
-      );
-    }
-  };
+  return (
+    <div>
+      <button
+        onClick={props.handleSaveSearch}
+        className={ButtonStyles.buttonPrimary}
+      >
+        Save Search
+      </button>
+      {props.saved ? <p>Search Saved!</p> : null}
 
-  setisActive(){
-      this.setState({
-          isActive:!this.state.isActive
-      })
-  }
+      <div className={`dropdown ${isActive ? "is-active" : ""}`}>
+        <div class="dropdown-trigger">
+          <li
+            onClick={() => {
+              setisActive(!isActive);
+            }}
+            style={{ borderStyle: "none", cursor: "pointer" }}
+            aria-haspopup="true"
+            aria-controls="dropdown-menu2"
+          >
+            <a>Saved Searches</a>
+            <span class="icon is-small">
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                alt="angle down"
+                style={{ marginLeft: "10px", width: "8px" }}
+              />
+            </span>
+          </li>
+        </div>
 
-  render() {
-    return (
-      <div>
-          <button onClick={this.handleSaveSearch} className={ButtonStyles.buttonPrimary}>Save Search</button>
-          {this.state.saved ? (
-          <p>Search Saved!</p>
-        ) : null}
-
-        
-
-        <div className={`dropdown ${this.state.isActive ? "is-active" : ""}`}>
-          <div class="dropdown-trigger">
-            <li
-              onClick={() => {
-                this.setisActive();
-              }}
-              style={{ borderStyle: "none", cursor: "pointer" }}
-              aria-haspopup="true"
-              aria-controls="dropdown-menu2"
-            >
-              <a>Saved Searches</a>
-              <span class="icon is-small">
-                <FontAwesomeIcon
-                  icon={faAngleDown}
-                  alt="angle down"
-                  style={{ marginLeft: "10px", width: "8px" }}
-                />
-              </span>
-            </li>
-          </div>
-          
-          <div class="dropdown-menu" id="dropdown-menu2" role="menu">
-            <div class="dropdown-content">
-            {this.state.existingEntries.slice(0, 10).map((entry) => {
-          return <p class="dropdown-item">{entry}</p>;
-        })}
-              
-              
-            </div>
+        <div class="dropdown-menu" id="dropdown-menu2" role="menu">
+          <div class="dropdown-content">
+            {props.existingEntries.slice((props.existingEntries.length - 10), props.existingEntries.length).map((entry) => {
+              return (
+                <button
+                  onClick={() => {
+                    props.handleSavedSearchLink(entry);
+                    setisActive(!isActive);
+                  }}
+                  class="dropdown-item"
+                >
+                  {entry}
+                </button>
+              );
+            })}
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
